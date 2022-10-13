bb91070b6921cd00871c3652da73d945fe23c916
徐锐 <2603029264@qq.com>
2022-10-14 00:23:20 +0800

    nest+mysql配置step

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Post } from '@nestjs/common';
 
 @Controller('article')
 export class ArticleController {
   @Get()
   queryArticle(): string {
     return '<h2>我是文章页面<h2>';
+  }
+
+  @Get('/add')
+  articleAdd(@Query() query): string {
+    console.log(query);
+    return query;
+  }
+
+  @Post('/editor')
+  articleEditor(): string {
+    return '123';
   }
 }
