ac4937370d9c13c827dd55027e45472ca0dc484f
徐锐 <2603029264@qq.com>
2022-10-15 00:56:15 +0800

    配置mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleController } from './article/article.controller';
 import { getConfig } from './utils';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
 
 console.log(getConfig);
 
@@ -14,8 +19,24 @@ console.log(getConfig);
       ignoreEnvFile: false,
       load: [getConfig],
     }),
+    // mysql连接
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123abcde',
+      database: 'blog',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
   ],
-  controllers: [AppController, ArticleController],
+  controllers: [AppController, ArticleController, UserController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly dataSource: DataSource) {
+    console.log(dataSource);
+  }
+}
