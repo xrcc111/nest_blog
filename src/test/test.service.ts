e5cd606c62f4fac50bf780966689dc06e5966444
徐锐 <2603029264@qq.com>
2022-10-17 00:29:51 +0800

    单个查询和批量查询

--- src/test/test.service.ts
+++ src/test/test.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Test } from './entities/test.entity';
 import { CreateTestDto } from './dto/create-test.dto';
 import { UpdateTestDto } from './dto/update-test.dto';
 
 @Injectable()
 export class TestService {
+  constructor(
+    @InjectRepository(Test)
+    private TestRepository: Repository<Test>,
+  ) {}
   create(createTestDto: CreateTestDto) {
     return 'This action adds a new test';
   }
 
-  findAll() {
-    return `This action returns all test`;
+  findAll(): Promise<Test[]> {
+    return this.TestRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} test`;
+  findOne(id: number): Promise<Test> {
+    debugger;
+    console.log(id);
+    return this.TestRepository.findOne({
+      where: {
+        id,
+      },
+    });
   }
 
   update(id: number, updateTestDto: UpdateTestDto) {
